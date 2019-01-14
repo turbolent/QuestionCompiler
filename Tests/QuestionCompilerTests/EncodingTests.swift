063ded95a70e145479f2f00bc9a3ce6baefa53c6
Bastian Müller <bastian@turbolent.com>
2019-01-14 07:11:02 -0800

    fix JSON encoding test

--- Tests/QuestionCompilerTests/EncodingTests.swift
+++ Tests/QuestionCompilerTests/EncodingTests.swift
@@ -14,86 +14,88 @@ class EncodingTests: XCTestCase {
         let root = person
             .incoming(movie, .hasCastMember)
 
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = .prettyPrinted
 
-        diffedAssertEqual(
-            String(data: try encoder.encode(root), encoding: .utf8),
-            """
-            {
-              "label" : {
-                "type" : "variable",
-                "id" : 0
-              },
-              "filter" : null,
-              "order" : null,
-              "type" : "node",
-              "edge" : {
-                "type" : "edge",
-                "subtype" : "conjunction",
-                "edges" : [
-                  {
-                    "label" : {
-                      "type" : "property",
-                      "name" : "isA",
-                      "url" : "http:\\/\\/example.org\\/property\\/isA"
-                    },
-                    "type" : "edge",
-                    "subtype" : "outgoing",
-                    "target" : {
-                      "label" : {
-                        "type" : "item",
-                        "name" : "person",
-                        "url" : "http:\\/\\/example.org\\/item\\/person"
-                      },
-                      "filter" : null,
-                      "order" : null,
-                      "type" : "node",
-                      "edge" : null
-                    }
+        if #available(OSX 10.13, *) {
+            diffJSON(
+                """
+                {
+                  "label" : {
+                    "type" : "variable",
+                    "id" : 0
                   },
-                  {
-                    "label" : {
-                      "type" : "property",
-                      "name" : "hasCastMember",
-                      "url" : "http:\\/\\/example.org\\/property\\/hasCastMember"
-                    },
-                    "source" : {
-                      "label" : {
-                        "type" : "variable",
-                        "id" : 1
-                      },
-                      "filter" : null,
-                      "order" : null,
-                      "type" : "node",
-                      "edge" : {
+                  "filter" : null,
+                  "order" : null,
+                  "type" : "node",
+                  "edge" : {
+                    "type" : "edge",
+                    "subtype" : "conjunction",
+                    "edges" : [
+                      {
                         "label" : {
                           "type" : "property",
-                          "name" : "hasName",
-                          "url" : "http:\\/\\/example.org\\/property\\/hasName"
+                          "name" : "isA",
+                          "url" : "http:\\/\\/example.org\\/property\\/isA"
                         },
                         "type" : "edge",
                         "subtype" : "outgoing",
                         "target" : {
                           "label" : {
-                            "type" : "value",
-                            "subtype" : "string",
-                            "value" : "Alien"
+                            "type" : "item",
+                            "name" : "person",
+                            "url" : "http:\\/\\/example.org\\/item\\/person"
                           },
                           "filter" : null,
                           "order" : null,
                           "type" : "node",
                           "edge" : null
                         }
+                      },
+                      {
+                        "label" : {
+                          "type" : "property",
+                          "name" : "hasCastMember",
+                          "url" : "http:\\/\\/example.org\\/property\\/hasCastMember"
+                        },
+                        "source" : {
+                          "label" : {
+                            "type" : "variable",
+                            "id" : 1
+                          },
+                          "filter" : null,
+                          "order" : null,
+                          "type" : "node",
+                          "edge" : {
+                            "label" : {
+                              "type" : "property",
+                              "name" : "hasName",
+                              "url" : "http:\\/\\/example.org\\/property\\/hasName"
+                            },
+                            "type" : "edge",
+                            "subtype" : "outgoing",
+                            "target" : {
+                              "label" : {
+                                "type" : "value",
+                                "subtype" : "string",
+                                "value" : "Alien"
+                              },
+                              "filter" : null,
+                              "order" : null,
+                              "type" : "node",
+                              "edge" : null
+                            }
+                          }
+                        },
+                        "type" : "edge",
+                        "subtype" : "incoming"
                       }
-                    },
-                    "type" : "edge",
-                    "subtype" : "incoming"
+                    ]
                   }
-                ]
-              }
-            }
-            """
-        )
+                }
+                """,
+                root
+            )
+        } else {
+            // Fallback on earlier versions
+        }
     }
 }
